f34eb90bcdd3fbd0dcbe7a1b8a58416747ce589a
Wes Modes <wmodes@gmail.com>
2022-12-27 13:09:22 -0800

    Refined Template

---
+++ lab3/js/lab.js
@@ -0,0 +1,21 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
